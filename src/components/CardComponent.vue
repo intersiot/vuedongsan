5fc05146c5296e452fc6adeea93c60778ef7b3d6
intersiot <75784873+intersiot@users.noreply.github.com>
2022-06-29 18:35:39 +0900

    Update CardComponent.vue
    부모데이터를 자식이 사용하는 방법 $emit, $event

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <img :src="원룸.image" class="room-img" alt="room0.jpg">
+    <!-- <h4 @click="$emit('openModal', 원룸.id)">{{ 원룸.title }}</h4> -->
     <h4>{{ 원룸.title }}</h4>
     <p>{{ 원룸.price }}원</p>
     <!-- <button @click="신고수[i]++">허위매물신고버튼</button> <span>신고수 : {{ 신고수[i] }}</span> -->
@@ -12,7 +13,12 @@ export default {
   name: 'CardComponent',
   props: {
     원룸: Object,
-  }
+  },
+  methods: {
+    send() {
+      this.$emit('openModal', this.원룸.id)
+    }
+  },
 }
 </script>
 
