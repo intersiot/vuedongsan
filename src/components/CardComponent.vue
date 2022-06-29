dea0b3e67270add078220b337f22728b865ce174
intersiot <75784873+intersiot@users.noreply.github.com>
2022-06-29 15:02:35 +0900

    Add files via upload

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img :src="원룸.image" class="room-img" alt="room0.jpg">
+    <h4>{{ 원룸.title }}</h4>
+    <p>{{ 원룸.price }}원</p>
+    <!-- <button @click="신고수[i]++">허위매물신고버튼</button> <span>신고수 : {{ 신고수[i] }}</span> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComponent',
+  props: {
+    원룸: Object,
+  }
+}
+</script>
+
+<style>
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+</style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="black-bg" v-if="모달창열렸니 == true">
+      <div class="white-bg">
+        <img :src="원룸들[누른거].image" class="room-img" />
+        <h4>{{ 원룸들[누른거].title }}</h4>
+        <p>{{ 원룸들[누른거].content }}</p>
+        <!-- <button v-on:click="모달창열렸니 = false">닫기</button> -->
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalComponent',
+  props: {
+    원룸들: Array,
+    누른거: Number,
+    모달창열렸니: Boolean,
+  },
+}
+</script>
+
+<style>
+
+</style>
