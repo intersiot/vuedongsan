82de22be0a390bb1a9f635a22dca90295933d68e
intersiot <75784873+intersiot@users.noreply.github.com>
2022-06-29 22:14:57 +0900

    Update DiscountComponent.vue
    mounted

--- src/components/DiscountComponent.vue
+++ src/components/DiscountComponent.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="discount">
-    <h4>지금 결제하면 20% 할인!</h4>
+    <h4>지금 결제하면 {{ amount }}% 할인!</h4>
   </div>  
 </template>
 
 <script>
 export default {
     name: 'DiscountComponent',
-
+    data() {
+      return {
+        amount: 30,
+      }
+    },
+    mounted() { // App.vue가 mount되고 나서 안의 코드 실행해주셈
+      setInterval(() => {
+        if (this.amount > 0) {
+          this.amount--;
+        } else if (this.amount <= 0) {
+          clearInterval // setInterval을 멈추는 함수
+        }
+      }, 1000)
+    },
 }
 </script>
 
